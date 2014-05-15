831591e8a897576ea5377bc4601dba8655a69a2b
mibewh <michael.whitman@fuse.net>
2014-05-15 08:42:57 -0400

    Added project

---
+++ Senior Project/src/wzh/game/Main.java
@@ -0,0 +1,46 @@
+package wzh.game;
+
+import java.io.File;
+
+import org.lwjgl.LWJGLUtil;
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+
+public class Main extends BasicGame{
+
+	public Main(String title) {
+		super(title);
+		// TODO Auto-generated constructor stub
+	}
+
+	public void render(GameContainer arg0, Graphics arg1) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void init(GameContainer arg0) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void update(GameContainer arg0, int arg1) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+	public static void main(String[] args) {
+		//Import system settings
+		System.setProperty("org.lwjgl.librarypath", new File(new File(System.getProperty("user.dir"), "lib/native"), LWJGLUtil.getPlatformName()).getAbsolutePath());
+		try {
+			AppGameContainer gc = new AppGameContainer(new Main("Test"));
+			gc.setDisplayMode(500, 500, false);
+			gc.start();
+		} catch (SlickException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+}
