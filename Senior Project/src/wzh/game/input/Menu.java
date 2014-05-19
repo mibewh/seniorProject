a0a597a7edc345dc68dfae0f967271d470f7e8a7
Jixiang Huang <Jixiang Huang@192.168.200.36>
2014-05-19 16:08:37 -0400

    menu font

--- Senior Project/src/wzh/game/input/Menu.java
+++ Senior Project/src/wzh/game/input/Menu.java
@@ -1,27 +1,44 @@
 package wzh.game.input;
 
+import java.awt.Font;
 import java.util.ArrayList;
 
+import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.TrueTypeFont;
 import org.newdawn.slick.state.StateBasedGame;
 
 import wzh.game.input.command.Command;
 
 public class Menu{
 	private int length;
+	private int x,y;
 	
-	private ArrayList<Command> commands = new ArrayList<Command>();
+	private ArrayList<Command> commands;
 	
-	public void update(GameContainer gc, StateBasedGame game, int delta) throws SlickException {
-		
-	}
-	public void render(GameContainer gc, StateBasedGame game, Graphics g) throws SlickException {
+	public Menu(int x, int y, ArrayList<Command> commands){
+		this.commands = commands;
+		this.x = x;
+		this.y = y;
 		for(Command i: commands){
 			length += 16;
-			g.fillRect(0, 0, 60, length);
-			g.drawString("TEST", 0, 0);
+		}
+	}
+	
+	public void update(GameContainer gc, StateBasedGame game, int delta) throws SlickException {
+		//Determine what is selected
+	}
+	public void render(GameContainer gc, StateBasedGame game, Graphics g) throws SlickException {
+		g.setColor(Color.white);
+		g.fillRect(x, y, 64, length);
+		g.setColor(Color.black);
+		TrueTypeFont f = new TrueTypeFont(new Font("", Font.PLAIN , 13), false);
+		g.setFont(f);
+		for(Command i: commands){
+			g.drawString("TEST", x, y);
+			y += 16;
 		}
 	}
 }
--- Senior Project/src/wzh/game/level/Level1.java
+++ Senior Project/src/wzh/game/level/Level1.java
@@ -1,5 +1,7 @@
 package wzh.game.level;
 
+import java.util.ArrayList;
+
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
@@ -11,10 +13,15 @@ import org.newdawn.slick.tiled.TiledMap;
 import wzh.game.Grid;
 import wzh.game.entity.Unit;
 import wzh.game.input.Cursor;
+import wzh.game.input.Menu;
+import wzh.game.input.command.Command;
+import wzh.game.input.command.Move;
+import wzh.game.input.command.Wait;
 
 public class Level1 extends BasicGameState {
 	
 	private Grid grid;
+	private Menu menu; //Temporary test menu
 
 	public void init(GameContainer gc, StateBasedGame game) throws SlickException {
 		grid = new Grid(new TiledMap("res/map.tmx"));
@@ -22,12 +29,19 @@ public class Level1 extends BasicGameState {
 		grid.add(new Unit(5,5,s.getSubImage(1, 0),grid,0));
 		grid.setCursor(new Cursor(7,7,grid));
 		grid.getCursor().setFocus(true);
+		ArrayList<Command> commands = new ArrayList<Command>();
+		commands.add(new Move());
+		commands.add(new Wait());
+		
+		menu = new Menu(0,0, commands);
+		
 	}
 
 	public void render(GameContainer gc, StateBasedGame game, Graphics g) throws SlickException {
 		//Change 3rd and 4th 0's to allow for screen scrolling
 		g.scale(2, 2);
 		grid.render(gc, game, g);
+		menu.render(gc,game,g);
 	}
 
 	public void update(GameContainer gc, StateBasedGame game, int delta) throws SlickException {
