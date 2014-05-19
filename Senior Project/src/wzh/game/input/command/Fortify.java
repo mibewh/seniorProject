ea5c2eb64885edcf1b3fc9f75f1a7dfa0d46e2e7
Harrison <Harrison@192.168.200.35>
2014-05-19 14:53:15 -0400

    Fortify class created

---
+++ Senior Project/src/wzh/game/input/command/Fortify.java
@@ -0,0 +1,13 @@
+package wzh.game.input.command;
+
+public class Fortify extends Command {
+
+	public Fortify(String commandName){
+		super(commandName);
+		commandName="Fortify";
+	}
+	
+	public void select(){
+		
+	}
+}
