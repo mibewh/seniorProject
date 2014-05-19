35507bca69f2f26b855be00574d10783770b2326
mibewh <michael.whitman@fuse.net>
2014-05-19 11:32:03 -0400

    Worked on grid/entity rendering

--- Senior Project/src/wzh/game/entity/Unit.java
+++ Senior Project/src/wzh/game/entity/Unit.java
@@ -1,8 +1,23 @@
 package wzh.game.entity;
 
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
+
+import wzh.game.Grid;
 
 public class Unit extends Entity {
-	private Image sprite;
+	private int faction;
+	private int movePoints;
 	
+	public Unit(int x, int y, Image img, Grid g, int faction) {
+		super(x,y,img,g);
+		this.faction = faction;
+	}
+	public void render(GameContainer gc, StateBasedGame game, Graphics g) throws SlickException {
+		super.render(gc, game, g);
+		System.out.println("Drew");
+	}
 }
