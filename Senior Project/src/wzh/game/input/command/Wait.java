7129019728ddf0c841afc6e4f9b8789fb59ac0ab
Harrison <Harrison@192.168.200.35>
2014-05-19 14:41:18 -0400

    added jabberwoks

---
+++ Senior Project/src/wzh/game/input/command/Wait.java
@@ -0,0 +1,14 @@
+package wzh.game.input.command;
+
+public class Wait extends Command {
+	
+	public Wait(String commandName){
+		super(commandName);
+		commandName="Wait";
+	}
+	
+	public void select(){
+		
+	}
+
+}
