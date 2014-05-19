6b8df1f091fd589ec4dd38a1de5b7605ba60790e
Jixiang Huang <Jixiang Huang@192.168.200.36>
2014-05-19 13:29:32 -0400

    Started Menu

---
+++ Senior Project/src/wzh/game/input/Menu.java
@@ -0,0 +1,24 @@
+package wzh.game.input;
+
+import java.util.ArrayList;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
+
+public class Menu{
+	ArrayList commands = new ArrayList();
+	
+	commands.add("Move");
+	commands.add("")
+	
+	public void update(GameContainer gc, StateBasedGame game, int delta) throws SlickException {
+		commands.add("Move");
+		commands.add("Attack");
+		commands.add("Wait");
+	}
+	public void render(GameContainer gc, StateBasedGame game, Graphics g) throws SlickException {
+		
+	}
+}
