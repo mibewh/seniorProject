0928a32ba6147dee4b022ca4b6c455c6c4c1f1ed
Jixiang Huang <Jixiang Huang@192.168.200.36>
2014-05-20 13:37:47 -0400

    revert

--- Senior Project/src/wzh/game/input/Menu.java
+++ Senior Project/src/wzh/game/input/Menu.java
@@ -7,7 +7,6 @@ import java.util.ArrayList;
 import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
-import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.TrueTypeFont;
 import org.newdawn.slick.state.StateBasedGame;
@@ -22,14 +21,26 @@ public class Menu{
 	
 	private final String FONT_STYLE = "Minecraft";
 	
-	private int selected;	
 	private ArrayList<Command> commands;
 	
 	public Menu(Cursor c , ArrayList<Command> commands, GameContainer gc){
 		this.commands = commands;
-		selected = 0;
-		x = c.getLoc().getX()*c.getSize()*2-32;
-		y = c.getLoc().getY()*c.getSize()*2-32;
+		if(c.getLoc().getX()*c.getSize()*2-64 >= 0 && c.getLoc().getY()*c.getSize()*2-32 >= 0){
+			x = c.getLoc().getX()*c.getSize()*2-64;
+			y = c.getLoc().getY()*c.getSize()*2-32;
+		}
+		else if(c.getLoc().getX()*c.getSize()*2-64 <= 0 && c.getLoc().getY()*c.getSize()*2-32 >= 0){
+			x = c.getLoc().getX()*c.getSize()*2+32;
+			y = c.getLoc().getY()*c.getSize()*2-32;
+		}
+		else if(c.getLoc().getX()*c.getSize()*2-64 >= 0 && c.getLoc().getY()*c.getSize()*2-32 <= 0){
+			x = c.getLoc().getX()*c.getSize()*2-64;
+			y = c.getLoc().getY()*c.getSize()*2+32;
+		}
+		else if(c.getLoc().getX()*c.getSize()*2-64 <= 0 && c.getLoc().getY()*c.getSize()*2-32 <= 0){
+			x = c.getLoc().getX()*c.getSize()*2+32;
+			y = c.getLoc().getY()*c.getSize()*2+32;
+		}
 		cursor = c;
 		InputStream is = ResourceLoader.getResourceAsStream("Fonts/Minecraft.ttf");
 		try {
@@ -43,19 +54,6 @@ public class Menu{
 	
 	public void update(GameContainer gc, StateBasedGame game, int delta) throws SlickException {
 		//TODO Determine what is selected
-		if(gc.getInput().isKeyPressed(Input.KEY_UP) || gc.getInput().isKeyPressed(Input.KEY_W)) {
-			selected--;
-		}
-		else if(gc.getInput().isKeyPressed(Input.KEY_DOWN) || gc.getInput().isKeyPressed(Input.KEY_S)) {
-			selected++;
-		}
-		if(selected<0) selected=commands.size()-1;
-		else if (selected>=commands.size()) selected=0;
-		if(gc.getInput().isKeyPressed(Input.KEY_SPACE)) {
-			
-		}
-		
-		
 	}
 	public void render(GameContainer gc, StateBasedGame game, Graphics g) throws SlickException {
 		g.setColor(Color.white);
