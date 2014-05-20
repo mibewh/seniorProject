8bbbeb6f0794b60d3b326d0e634b18c99e60f40f
mibewh <michael.whitman@fuse.net>
2014-05-20 09:27:23 -0400

    Location getDirectionToward method

--- Senior Project/src/wzh/game/Location.java
+++ Senior Project/src/wzh/game/Location.java
@@ -4,6 +4,11 @@ import java.util.ArrayList;
 
 public class Location {
 	
+	public static final int UP = 0;
+	public static final int LEFT = 90;
+	public static final int DOWN = 180;
+	public static final int RIGHT = 270;
+	
 	private int x,y;
 	
 	public Location(int x, int y) {
@@ -23,5 +28,13 @@ public class Location {
 		locs.add(new Location(x,y+1));
 		locs.add(new Location(x,y-1));
 		return locs;
+	}
+	public int getDirectionToward(Location loc) {
+		int xDif = loc.getX()-x;
+		int yDif = loc.getY()-y;
+		if(xDif<0 && Math.abs(xDif)>Math.abs(yDif)) return Location.LEFT;
+		else if(xDif>0 && Math.abs(xDif)>Math.abs(yDif)) return Location.RIGHT;
+		else if(yDif<0 && Math.abs(yDif)>Math.abs(xDif)) return Location.UP;
+		else return Location.DOWN;
 	}
 }
