9fd392838ca3faeee68db957f57ff62499d18cfa
Harrison <Harrison@192.168.200.35>
2014-05-21 12:08:05 -0400

    Made attack method in Swordsman

--- Senior Project/src/wzh/game/entity/unit/Swordsman.java
+++ Senior Project/src/wzh/game/entity/unit/Swordsman.java
@@ -1,14 +1,25 @@
 package wzh.game.entity.unit;
 
 import org.newdawn.slick.Image;
+
 import wzh.game.Grid;
 
 public class Swordsman extends Unit{ 
 
 	public Swordsman(int x, int y, Image img, Grid g, int faction){
 		super(x, y, img, g, faction);
-        movePoints=5;
-        attack= hp/10+(int)Math.sqrt(hp)*2;
-        
+	}
+	
+	@Override 
+	public void attack(Unit other){
+			double startAttack = attack;
+			if(other instanceof Spearman) {
+				attack *= 2;
+			}
+			else if(other instanceof Horseman) {
+				attack /= 2;
+			}
+			super.attack(other);
+			attack = startAttack;
 	}
 }
