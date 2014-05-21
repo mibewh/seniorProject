9888def1955a5ef2fc7540c6a9ce5459ad494441
Harrison <Harrison@192.168.200.35>
2014-05-21 11:56:59 -0400

    Changed swordsman constructer a bit

--- Senior Project/src/wzh/game/entity/unit/Swordsman.java
+++ Senior Project/src/wzh/game/entity/unit/Swordsman.java
@@ -3,14 +3,12 @@ package wzh.game.entity.unit;
 import org.newdawn.slick.Image;
 import wzh.game.Grid;
 
-public class Swordsman extends Unit{
-	
-	private int movePoints=5;
-	private int hp=100;
-	private int attack= hp/10+(int)Math.sqrt(hp)*2;
-	private int defense=1; 
+public class Swordsman extends Unit{ 
 
-	public Swordsman(int x, int y, Image img, Grid g, int faction) {
+	public Swordsman(int x, int y, Image img, Grid g, int faction){
 		super(x, y, img, g, faction);
+        movePoints=5;
+        attack= hp/10+(int)Math.sqrt(hp)*2;
+        
 	}
 }
