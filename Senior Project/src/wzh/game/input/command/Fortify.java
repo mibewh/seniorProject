d3246abeb8ea33818df781e965d2c05a568c31a2
Harrison <Harrison@192.168.200.35>
2014-05-21 09:24:26 -0400

    Fixed constrctors for commands

--- Senior Project/src/wzh/game/input/command/Fortify.java
+++ Senior Project/src/wzh/game/input/command/Fortify.java
@@ -5,11 +5,17 @@ import wzh.game.input.Cursor;
 
 public class Fortify extends Command {
 
+	private Unit u;
+	private Cursor c;
+
 	public Fortify(Unit u, Cursor c){
 		super();
+		this.u=u;
+		this.c=c;
 		commandName="Fortify";
 	}
 	
 	public void select(){
+		
 	}
 }
