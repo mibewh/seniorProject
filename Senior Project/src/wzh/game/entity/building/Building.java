b28e5c720dff3f5e6d3dcc2e852772a85f6199be
Jixiang Huang <Jixiang Huang@192.168.200.36>
2014-05-22 10:09:47 -0400

    some village stuff

--- Senior Project/src/wzh/game/entity/building/Building.java
+++ Senior Project/src/wzh/game/entity/building/Building.java
@@ -7,16 +7,19 @@ import wzh.game.entity.Entity;
 import wzh.game.input.Menu;
 
 public abstract class Building extends Entity {
+	protected int faction;
 	
 	public Building(int x, int y, Image img, Grid g) {
 		super(x, y, img, g);
 	}
-	
+	public int getFaction() {
+		return faction;
+	}
 	public int getFortification(){
 		return 0;
 	}
 	public Menu getMenu() {
 		//TODO Implement building menus
 		return null;
-	}
+	} 
 }
--- Senior Project/src/wzh/game/entity/building/Village.java
+++ Senior Project/src/wzh/game/entity/building/Village.java
@@ -1,16 +1,43 @@
 package wzh.game.entity.building;
 
+import java.util.ArrayList;
+
 import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.SpriteSheet;
 
 import wzh.game.Grid;
+import wzh.game.Location;
+import wzh.game.entity.unit.Archer;
+import wzh.game.entity.unit.Unit;
 
 public abstract class Village extends Building {
 	public Village(int x, int y, Image img, Grid g) {
 		super(x, y, img, g);
+	}
+	
+	/*
+	 *public void addMoney()
+	 */
+	
+	public void goGray() {
+	 }
+	
+	public void goColor() {
+		try {
+			SpriteSheet ss = new SpriteSheet("SpriteSheetz.png",16,16);
+			if(faction==1)
+				sprite = ss.getSubImage(2, 1);
+			else
+				sprite = ss.getSubImage(1, 1);
+		} catch (SlickException e) {
+			e.printStackTrace();
 		}
+		
+	}
+
 	
 	public int getFortification(){
 		return 1;
 	}
-	//TODO Check THE VILLAGE fortification of 0?
 }
