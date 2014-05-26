40a6251a547a2f2e36c1da7895437ffaa5ce9d86
Harrison <Harrison@192.168.200.35>
2014-05-26 09:30:39 -0400

    Syncccc

--- Senior Project/src/wzh/game/input/menuCommands/Instructions.java
+++ Senior Project/src/wzh/game/input/menuCommands/Instructions.java
@@ -23,5 +23,5 @@ public class Instructions {
 	  As of right now, there is four diffent types of units your army can field. Each have their own advantages and disadvantages:
 	  the Spearman - this worthless unit are cheap to field and are good against horses. Otherwise they suck. Even against themselves. Use they
 	  early on when you don't have as gold in order to quickly flood the fields.
-	  the Swordsman -  this manly unit is hardy and serves its purpose as a hearty frontline troop.
+	  the Swordsman -  this manly unit is hardy and serves its purpose as a hearty frontline troop. */
 }
