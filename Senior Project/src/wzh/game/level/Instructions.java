ca68359fa21707ec932720c848e6c7641d3dd324
Harrison <Harrison@192.168.200.35>
2014-05-26 16:50:00 -0400

    Added instructions menu to main manu

--- Senior Project/src/wzh/game/level/Instructions.java
+++ Senior Project/src/wzh/game/level/Instructions.java
@@ -2,35 +2,45 @@ package wzh.game.level;
 
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
 public class Instructions extends BasicGameState {
 	
+	private int curPage;
+	private Image page1;
+	private Image page2;
+	private Image page3;
 	
-	
-	public int getId() {
+	public int getID() {
 		return 11;
 	}
 
 	public void init(GameContainer container, StateBasedGame game) throws SlickException {
-		// TODO Auto-generated method stub
-		
+		curPage=1;
+		page1=new Image("MenuInstructions.png");
+		page2=new Image("MenuInstructions2.png");
+		page3=new Image("MenuInstructions3.png");
 	}
 
-	public void render(GameContainer container, StateBasedGame game, Graphics g) throws SlickException {
-		// TODO Auto-generated method stub
-		
+	public void render(GameContainer gc, StateBasedGame game, Graphics g) throws SlickException {
+		if(curPage==1)
+			page1.draw();
+		if(curPage==2)
+			page2.draw();
+		if(curPage==3)
+			page3.draw();
 	}
 
-	public void update(GameContainer container, StateBasedGame game, int delta) throws SlickException {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public int getID() {
-		// TODO Auto-generated method stub
-		return 0;
+	public void update(GameContainer gc, StateBasedGame game, int delta) throws SlickException {
+		if(gc.getInput().isKeyPressed(Input.KEY_SPACE))
+				curPage++;
+		if(curPage>=4){
+			curPage=1;
+			game.enterState(0);
+		}
 	}
 }
