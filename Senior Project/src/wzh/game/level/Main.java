c43f4040ea73a78fd59a0d43a0783188ba7396ba
mibewh <michael.whitman@fuse.net>
2014-05-26 17:11:59 -0400

    Minimaps for existent levels

--- Senior Project/src/wzh/game/level/Main.java
+++ Senior Project/src/wzh/game/level/Main.java
@@ -13,10 +13,10 @@ public class Main extends StateBasedGame{
 	public void initStatesList(GameContainer arg0) throws SlickException {
 		addState(new MainMenu());
 		addState(new Level("maps/map2.tmx",1));
-//		addState(new Level("maps/map3.tmx",2));
-//		addState(new Level("maps/map4.tmx",3));
+		addState(new Level("maps/map3.tmx",2));
+		addState(new Level("maps/map4.tmx",3));
 //		addState(new Level("maps/map5.tmx",4));
-//		addState(new Level("maps/map6.tmx",5));
+		addState(new Level("maps/map6.tmx",5));
 //		addState(new Instructions());
 		addState(new Play());
 	}
